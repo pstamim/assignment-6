b4c383e67990545c17c3201ec1632afd6934be82
pstamim <pstamim723@gmail.com>
2026-09-26 03:02:13 +0600

    sort button declear

--- src/app/myplane/page.tsx
+++ src/app/myplane/page.tsx
@@ -14,7 +14,8 @@ import { toast } from 'react-toastify';
 const Page = () => {
     const { addplane, setaddplane, saveExercise, setSaveExercise } = useContext(ExerciseContext)
     const [addtab, setaddtab] = useState('Today’s Plan')
-    const [sortby,setSortby] = useState('duration')
+
+    const [sortby, setSortby] = useState('duration')
 
     const handleremove = (id: number) => {
         if (addtab === 'Today’s Plan') {
@@ -22,16 +23,29 @@ const Page = () => {
             toast.error("Remove today's plan")
         } else {
             setSaveExercise(saveExercise.filter((exercise: IExercise) => exercise.id !== id))
-
             toast.error('Remove Save latter')
         }
-
-
-
 
     }
 
     const display = addtab === 'Today’s Plan' ? addplane : saveExercise
+    const sortedDisplay = [...display].sort((a, b) => {
+
+        if (sortby === 'duration') {
+            return a.duration - b.duration;
+        }
+
+        if (sortby === 'calories') {
+            return a.caloriesBurned - b.caloriesBurned;
+        }
+
+        if (sortby === 'rating') {
+            return a.rating - b.rating;
+        }
+
+        return 0;
+
+    });
     return (
         <div className='flex justify-between container mx-auto mt-10 '>
             <div className='space-y-3'>
@@ -42,33 +56,49 @@ const Page = () => {
                 <div className='flex justify-center gap-100 mt-10 border border-slate-800 bg-[#13161D] rounded-2xl p-10 pr-100 '>
                     <div>
                         <p className='text-[#8A92A0]'>Exercises</p>
-                        <h1 className='text-5xl font-bold text-[#C2F800]'>{display.length}</h1>
+                        <h1 className='text-5xl font-bold text-[#C2F800]'>{sortedDisplay.length}</h1>
                     </div>
                     <div className='border-l border-[#242833] pl-5'>
                         <p className='text-[#8A92A0] border-l border-slate-800'>Minutes</p>
-                        <h1 className='text-5xl font-bold'>{display.map(time => time.duration).reduce((acc, sum) => {
+                        <h1 className='text-5xl font-bold'>{sortedDisplay.map(time => time.duration).reduce((acc, sum) => {
                             return acc + sum
                         }, 0)}</h1>
                     </div>
                     <div className='border-l border-[#242833] pl-5'>
                         <p className='text-[#8A92A0]'>Calories</p>
-                        <h1 className='text-5xl font-bold'>{display.map(calorise => calorise.caloriesBurned).reduce((acc, sum) => {
+                        <h1 className='text-5xl font-bold'>{sortedDisplay.map(calorise => calorise.caloriesBurned).reduce((acc, sum) => {
                             return acc + sum
                         }, 0)}</h1>
                     </div>
                 </div>
-                <div className='flex w-fit items-center rounded-xl border border-[#242833] bg-[#12151b] p-1 mt-10'>
-                    <button onClick={() => setaddtab('Today’s Plan')}
-                        className={`rounded-lg px-5 py-2 cursor-pointer text-xs font-medium transition ${addtab === 'Today’s Plan' ? "bg-[#20252e] text-white" : "text-slate-500 hover:text-white"}`}>Today’s Plan</button>
-                    <button onClick={() => setaddtab('Saved')}
-                        className={`rounded-lg px-5 py-2 cursor-pointer text-xs font-medium transition
+                <div className='flex justify-between'>
+
+                    <div className='flex  w-fit items-center rounded-xl border border-[#242833] bg-[#12151b] p-1 mt-10'>
+                        <button onClick={() => setaddtab('Today’s Plan')}
+                            className={`rounded-lg px-5 py-2 cursor-pointer text-xs font-medium transition ${addtab === 'Today’s Plan' ? "bg-[#20252e] text-white" : "text-slate-500 hover:text-white"}`}>Today’s Plan</button>
+                        <button onClick={() => setaddtab('Saved')}
+                            className={`rounded-lg px-5 py-2 cursor-pointer text-xs font-medium transition
                             ${addtab === 'Saved' ? 'bg-[#20252e] text-white' : "text-slate-500 hover:text-white"}
                          hover:text-white`}>Saved</button>
-                         
+                    </div>
+
+                    <div className="flex items-center gap-3">
+                        <label className="text-sm text-[#8A92A0]">sortby</label>
+                        <select
+                            value={sortby}
+                            onChange={(e) => setSortby(e.target.value)}
+                            className="cursor-pointer rounded-xl border border-slate-800 bg-[#13161D] px-4 py-3 text-sm text-white outline-none focus:border-[#C2F800]">
+
+                            <option value="duration">duration</option>
+                            <option value="calories">calories</option>
+                            <option value="rating">rating</option>
+
+                        </select>
+                    </div>
                 </div>
 
                 <div className='flex flex-col gap-4 mt-10'>
-                    {display.length === 0 ? (
+                    {sortedDisplay.length === 0 ? (
                         <div className="flex min-h-[285px] flex-col items-center justify-center rounded-2xl border border-dashed border-slate-800 bg-[#0D0F12] p-6 text-center">
                             <h2 className="text-2xl font-bold text-white">
                                 NOTHING HERE YET  </h2>
@@ -81,7 +111,7 @@ const Page = () => {
                         </div>
 
                     ) : (
-                        display.map((exercise) => <div className='flex items-center justify-between rounded-2xl border border-slate-800 bg-[#14171D] p-5' key={exercise.id}>
+                        sortedDisplay.map((exercise) => <div className='flex items-center justify-between rounded-2xl border border-slate-800 bg-[#14171D] p-5' key={exercise.id}>
                             <div className='flex justify-between items-center gap-5'>
 
                                 <div className='h-20 w-32 shrink-0 overflow-hidden rounded-2xl'>
